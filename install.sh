99ff201aca3354e7b11546a6ef2797c32e5fe647
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2017-05-14 10:52:38 +0200

    udpate node, yarn

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #sudo xcodebuild -license accept
 
 echo "installing nvm:"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 source ~/.nvm/nvm.sh
 
 NODE=v6
