aebf4f9db05f385548273dfc9a4c8dbedaa6e8fe
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2023-02-25 23:03:53 +0100

    zsh install indents

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,15 +2,17 @@
 pushd $HOME/.zsh
 
 if [ ! -d zsh-autosuggestions ]; then
-    git clone https://github.com/zsh-users/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-autosuggestions
 fi
 
 if [ ! -d zsh-syntax-highlighting ]; then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting
 fi
 
 if [ ! -d zsh-history-substring-search ]; then
-    git clone https://github.com/zsh-users/zsh-history-substring-search
+  git clone https://github.com/zsh-users/zsh-history-substring-search
+fi
+
 if [ ! -d gradle-completion ]; then
   git clone https://github.com/gradle/gradle-completion
 fi
