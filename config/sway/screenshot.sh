5abec19af26cf11e3d3f0bc36604324507786907
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2023-10-24 23:47:10 +0200

    move to grimshot for screenshots

--- config/sway/screenshot.sh
+++ config/sway/screenshot.sh
@@ -1,20 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 case $1 in
     s)
-    grim -g "$(slurp)" $HOME/Dropbox/Screenshots/$(date +"%Y-%m-%dT%H:%M:%SZ_grim.png")
+    grimshot save area \
+        $HOME/Dropbox/Screenshots/$(date +"%Y-%m-%dT%H:%M:%SZ_grim.png")
     ;;
     d)
-    grim -o $(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name') \
+    grimshot save output \
         $HOME/Dropbox/Screenshots/$(date +"%Y-%m-%dT%H:%M:%SZ_grim.png")
     ;;
     w)
-    grim -g "$(swaymsg -t get_tree | jq -j '.. | select(.type?) | select(.focused).rect | "\(.x),\(.y) \(.width)x\(.height)"')" \
+    grimshot save window \
         $HOME/Dropbox/Screenshots/$(date +"%Y-%m-%dT%H:%M:%SZ_grim.png")
     ;;
     p)
     grim -g "$(slurp -p)" -t ppm - | convert - -format '%[pixel:p{0,0}]' txt:-
     ;;
     *)
-    echo "usage: ./screenshot.sh s|d|f"
+    echo "usage: ./screenshot.sh s|d|w"
     ;;
 esac
