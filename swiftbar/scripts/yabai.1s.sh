831de5cbb9bd601c92eef164cbb5a4fe96b21d5a
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2024-01-07 10:13:49 +0100

    simplify yabai.1s.sh

--- swiftbar/scripts/yabai.1s.sh
+++ swiftbar/scripts/yabai.1s.sh
@@ -2,31 +2,26 @@
 
 export PATH=/usr/local/bin:$PATH
 
+if [[ "$1" = "start" ]]; then
+    yabai --start-service
+    skhd --start-service
+elif [[ "$1" = "restart" ]]; then
+    yabai --restart-service
+    skhd --restart-service
+elif [[ "$1" = "stop" ]]; then
+    yabai --stop-service
+    skhd --stop-service
+fi
+
 # check if its running
-pgrep yabai 2>&1 1> /dev/null
+pgrep yabai 1>/dev/null 2>&1
 if [[ $? -eq 0 ]]; then
-    if [[ "$1" = "restart" ]]; then
-        yabai --restart-service
-        skhd --restart-service
-    fi
-
-    if [[ "$1" = "stop" ]]; then
-        yabai --stop-service
-        skhd --stop-service
-    fi
-
     echo $(yabai -m query --spaces --display | jq -r 'sort_by(.index) | map(select(."has-focus" == true)) | first | .index')
     echo "---"
     echo "Restart yabai & skhd | bash='$0' param1=restart terminal=false"
     echo "Stop yabai & skhd | bash='$0' param1=stop terminal=false"
 else
-
-    if [[ "$1" = "restart" ]]; then
-        yabai --start-service
-        skhd --start-service
-    fi
-
     echo n/a
     echo "---"
-    echo "Start yabai & skhd | bash='$0' param1=restart terminal=false"
+    echo "Start yabai & skhd | bash='$0' param1=start terminal=false"
 fi
