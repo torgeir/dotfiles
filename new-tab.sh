9b24f5692fcb742739840ab2c7038c3c077d33b7
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2024-05-07 22:17:59 +0200

    simplify term binds, s/Firefox/Firefox Developer Edition

--- new-tab.sh
+++ new-tab.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-open -a Firefox
+# open -a Firefox
+open -a Firefox\ Developer\ Edition
 sleep 0.2 # some room to let go of shift key, which opens previous tab
 osascript -e 'tell application "System Events" to keystroke "t" using command down'
