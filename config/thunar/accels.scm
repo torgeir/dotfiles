2dd1cee987f221c11d05c5870c5d38b223ff917c
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2025-12-18 20:05:17 +0100

    thunar extensions

---
+++ config/thunar/accels.scm
@@ -0,0 +1,3 @@
+(gtk_accel_path "<Actions>/ThunarWindow/open-parent" "Backspace")
+(gtk_accel_path "<Actions>/ThunarWindow/open-parent" "<Alt>Up")
+(gtk_accel_path "<Actions>/ThunarActionManager/open" "<Alt>Down")
